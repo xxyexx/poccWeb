0fb90854ca673de769f0aae3d58d42c361c79917
xxyexx <313140949@qq.com>
2016-03-16 17:18:23 +0800

    内部账号
    测试修改

--- SCNUPOCC/src/main/java/edu/scnu/action/MemberAction.java
+++ SCNUPOCC/src/main/java/edu/scnu/action/MemberAction.java
@@ -46,14 +46,6 @@ public class MemberAction extends ActionSupport{
 	
 	
 	//getter
-	public String getLoginId() {
-		return loginId;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
 	public String getMemberType() {
 		return memberType;
 	}
